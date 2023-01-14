096d0915680d074873e5b68b60bbf8c1fff5299b
patrickelectric <patrickelectric@users.noreply.github.com>
2023-01-14 12:18:53 +0000

    deploy: b0eaa0839df1196ee4e9a70b8953eb6fbcda1713

--- crates.js
+++ crates.js
@@ -1,1 +1,1 @@
-window.ALL_CRATES = ["bytes","crc_any","ping_rs"];
+window.ALL_CRATES = ["bytes","ping_rs"];
--- implementors/core/marker/trait.Copy.js
+++ implementors/core/marker/trait.Copy.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"ping_rs":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.66.0/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"struct\" href=\"ping_rs/struct.PingMessagePack.html\" title=\"struct ping_rs::PingMessagePack\">PingMessagePack</a>"]]
+"ping_rs":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.66.1/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"struct\" href=\"ping_rs/struct.PingMessagePack.html\" title=\"struct ping_rs::PingMessagePack\">PingMessagePack</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
