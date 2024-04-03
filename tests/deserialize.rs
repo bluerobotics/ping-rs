e86ff5ec8095604027353b96c76a6c52dad1e885
Raul Victor Trombin <raulvtrombin@gmail.com>
2024-04-03 14:51:21 -0300

    src: tests: Add checksum error while deconding test and update test for new pattern

--- tests/deserialize.rs
+++ tests/deserialize.rs
@@ -39,6 +39,23 @@ fn test_simple_deserialization() {
         panic!("Failed to use decoder with valid message");
     };
 
+    // Retry with a wrong receipt CRC
+    for byte in &buffer[0..buffer.len() - 2] {
+        dbg!(byte, &decoder.state);
+        assert!(matches!(
+            decoder.parse_byte(byte.clone()),
+            DecoderResult::InProgress
+        ));
+    }
+    assert!(matches!(
+        decoder.parse_byte(buffer[buffer.len() - 2]),
+        DecoderResult::InProgress
+    ));
+    assert!(matches!(
+        decoder.parse_byte(0x01), // force a crc error
+        DecoderResult::Error(ParseError::ChecksumError(_))
+    ));
+
     // Wrong CRC test
     let buffer: Vec<u8> = vec![
         0x42, 0x52, 0x02, 0x00, 0x06, 0x00, 0x00, 0x00, 0x05, 0x00, 0xa1, 0x01,
