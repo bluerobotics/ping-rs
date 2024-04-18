51ed31e533d063f6769adc253c1aa98db7a4748d
Raul Victor Trombin <raulvtrombin@gmail.com>
2024-04-18 14:04:18 -0300

    src: error: Add new errors and impl

--- src/error.rs
+++ src/error.rs
@@ -1,13 +1,23 @@
-use crate::decoder;
+use crate::{decoder, message::ProtocolMessage};
 
 #[derive(Debug)]
 pub enum PingError {
     Io(std::io::Error),
     ParseError(decoder::ParseError),
+    TokioBroadcastError(tokio::sync::broadcast::error::RecvError),
+    TokioMpscError(tokio::sync::mpsc::error::SendError<ProtocolMessage>),
+    JoinError,
+    TimeoutError,
 }
 
 impl From<std::io::Error> for PingError {
     fn from(err: std::io::Error) -> PingError {
         PingError::Io(err)
+    }
+}
+
+impl From<tokio_serial::Error> for PingError {
+    fn from(err: tokio_serial::Error) -> PingError {
+        PingError::Io(std::io::Error::new(std::io::ErrorKind::Other, err))
     }
 }
