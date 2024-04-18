521eb61a75b4cea3fe09f48541a4fa223a77b081
patrickelectric <patrickelectric@users.noreply.github.com>
2024-04-18 18:19:42 +0000

    deploy: 0609863127ce08e7624e593548a8052e452cbdb0

--- ping_rs/message/sidebar-items.js
+++ ping_rs/message/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"constant":["HEADER"],"struct":["ProtocolMessage"],"trait":["DeserializeGenericMessage","DeserializePayload","PingMessage","SerializePayload"]};
+window.SIDEBAR_ITEMS = {"constant":["HEADER"],"struct":["ProtocolMessage"],"trait":["DeserializeGenericMessage","DeserializePayload","MessageInfo","PingMessage","SerializePayload"]};
